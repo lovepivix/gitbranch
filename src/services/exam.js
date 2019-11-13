77cecf317324249ebc6ae31646fb896f14874255
lovepivix <lop.aethen@gmail.com>
2019-11-13 12:02:45 +0800

    'delete_master'

--- src/services/exam.js
+++
@@ -1,4 +0,0 @@
-import request from "@/utils/request";
-
-export const createExam = (params) => request.post("/exam/exam", params);
-export const getExam = () => request.get("/exam/exam");
--- theme.js
+++
@@ -1,5 +0,0 @@
-export default {
-   "primary-color": "#f00"
-}
-    
- 
